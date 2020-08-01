68085bd3573e204a48430e25cfd1efcb63ff25db
BTower Labz <labz@btower.net>
2020-08-01 11:18:00 +0000

    updated infratest

--- .infratest/default/main.tf
+++ .infratest/default/main.tf
@@ -5,12 +5,20 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_default_route_table" "main" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+  tags = {
+    Name = "${var.name_prefix}-rt"
+  }
+}
+
 module "subnet" {
   source = "../../"
   cidr   = "10.0.1.0/24"
   az     = data.aws_availability_zone.az.name
   vpc_id = aws_vpc.main.id
+  rt_id  = aws_default_route_table.main.id
   tags = {
     Name = "${var.name_prefix}-subnet"
-  }  
+  }
 }
